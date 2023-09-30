0f2b55f87ad62c2ae78bdb733760d4a3e5217279
Derek Ashmore <derek.ashmore@dvtconsulting.com>
2023-09-30 06:01:50 -0400

    Changed the title

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('terraform-docs.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('terraform-docs.generate', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from terraform-docs!');
+		vscode.window.showInformationMessage('Generating documentation using terraform-docs!');
 	});
 
 	context.subscriptions.push(disposable);
